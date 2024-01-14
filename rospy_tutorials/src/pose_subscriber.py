5bf9793de423dffc35269961206292cf3078bd94
oveliz <155999511+oveliz@users.noreply.github.com>
2024-01-14 19:55:29 +0100

    Create pose_subscriber.py

---
+++ rospy_tutorials/src/pose_subscriber.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import rospy
+from turtlesim.msg import Pose
+
+def pose_callback(pose):
+    rospy.loginfo("Turtle Pose: x: {}, y: {}, theta: {}".format(pose.x, pose.y, pose.theta))
+
+def listener():
+    rospy.init_node('turtle_pose_subscriber', anonymous=True)
+    rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
